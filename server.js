ba2f4687a76325e09f5654242202894148b2de5b
Pratiksha Jadhav <nj626709@gmail.com>
2026-03-11 21:40:24 +0530

    fix email transport

--- server.js
+++ server.js
@@ -61,13 +61,15 @@ app.post("/contact", async (req, res) => {
     await newMessage.save();
 
     /* Send Email */
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-      }
-    });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
